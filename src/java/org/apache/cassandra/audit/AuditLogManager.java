2d4ba0a6ffc570a1bc5b2d1089caaf631758ba24
sumanthpasupuleti <sumanth.pasupuleti.is@gmail.com>
2018-09-01 02:01:20 -0700

    Change to filter any virtual keyspaces from getting audit logged

--- src/java/org/apache/cassandra/audit/AuditLogManager.java
+++ src/java/org/apache/cassandra/audit/AuditLogManager.java
@@ -34,6 +34,7 @@ import org.apache.cassandra.cql3.CQLStatement;
 import org.apache.cassandra.cql3.QueryHandler;
 import org.apache.cassandra.cql3.QueryOptions;
 import org.apache.cassandra.cql3.statements.BatchStatement;
+import org.apache.cassandra.db.virtual.VirtualKeyspaceRegistry;
 import org.apache.cassandra.exceptions.AuthenticationException;
 import org.apache.cassandra.exceptions.ConfigurationException;
 import org.apache.cassandra.exceptions.UnauthorizedException;
@@ -127,13 +128,18 @@ public class AuditLogManager
         return SchemaConstants.isLocalSystemKeyspace(keyspaceName);
     }
 
+    private boolean isVirtualKeyspace(String keyspaceName)
+    {
+        return VirtualKeyspaceRegistry.instance.getKeyspaceNullable(keyspaceName) != null;
+    }
+
     /**
      * Logs AuditLogEntry to standard audit logger
      * @param logEntry AuditLogEntry to be logged
      */
     private void logAuditLoggerEntry(AuditLogEntry logEntry)
     {
-        if ((logEntry.getKeyspace() == null || !isSystemKeyspace(logEntry.getKeyspace()))
+        if ((logEntry.getKeyspace() == null || (!isSystemKeyspace(logEntry.getKeyspace()) && !isVirtualKeyspace(logEntry.getKeyspace())))
             && !filter.isFiltered(logEntry))
         {
             auditLogger.log(logEntry);
